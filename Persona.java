ac2e6133a960aaa7bd778d4ce6dab3d38c542b1b
MIguelAngelMoralesFuentes <miguel.morales.fuentes@upvm.edu.mx>
2023-10-25 15:16:59 -0600

    srreglo persona se ha agregado

---
+++ Persona.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arregloshugo;
+
+
+public class Persona {
+private int num;
+private String nombre;
+private int edad;
+
+public Persona()
+{
+    
+}
+        
+public Persona(int num, String nombre, int edad)
+{
+    this.num = num;
+    this.nombre = nombre;
+    this.edad = edad;
+}
+
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+        
+}
